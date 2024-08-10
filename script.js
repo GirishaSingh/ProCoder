97c2499a6a237ea682d42f7e56aeac345d416d1b
Girisha Singh <120296791+GirishaSingh@users.noreply.github.com>
2024-08-10 11:06:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Initialize CodeMirror on the textarea with id 'code-editor'
     var editor = CodeMirror.fromTextArea(document.getElementById('code-editor'), {
-        lineNumbers: true,  // Show line numbers
-        mode: 'javascript', // Set the mode for syntax highlighting
-        theme: 'material'   // Set the theme (you can change this to any other theme you like)
+        lineNumbers: true,  
+        mode: 'javascript', 
+        theme: 'material'  
     });
 });
