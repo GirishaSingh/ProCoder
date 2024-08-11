4936a5324f6249076eb28a5d73702ac2c5fc0517
Girisha Singh <120296791+GirishaSingh@users.noreply.github.com>
2024-08-11 12:01:26 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var editor = CodeMirror.fromTextArea(document.getElementById('code-editor'), {
-        lineNumbers: true,  
-        mode: 'javascript', 
-        theme: 'material'  
-    });
-});
